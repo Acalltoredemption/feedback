91dc51ba016c661eb34c3b5e99c2a5a4177dbe55
rob oren <acalltoredemption@gmail.com>
2022-05-22 12:34:50 -0400

    feedback components functional - using delete method via prop drilling

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum lorem ipsum lorem ipsum'
+    },
+    {
+        id: 2,
+        rating: 5,
+        text: 'Lorem ipsum lorem ipsum lorem ipsum'
+    },
+    {
+        id: 3,
+        rating: 5,
+        text: 'Lorem ipsum lorem ipsum lorem ipsum'
+    },
+    {
+        id: 4,
+        rating: 9,
+        text: 'Lorem ipsum lorem ipsum lorem ipsum'
+    },
+];
+
+export default FeedbackData;

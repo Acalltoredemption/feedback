7a59ce4fe9eb0b8f3cace1659e3620a92ca0ddf9
rob oren <acalltoredemption@gmail.com>
2022-05-22 16:14:55 -0400

    able to add new feedback to app level state and implemented fade animations with framer motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,6 +1,6 @@
 import FeedbackItem from "./FeedbackItem"
 import PropTypes from 'prop-types'
-
+import {motion, AnimatePresence} from 'framer-motion'
 
 function FeedbackList({feedback, handleDelete}) {
     if(!feedback || feedback.length === 0){
@@ -8,9 +8,16 @@ function FeedbackList({feedback, handleDelete}) {
     }
   return (
     <div className="feedback-list">
+      <AnimatePresence>
         {feedback.map((item) => (
+          <motion.div initial={{opacity: 0}}
+           key={item.id}
+           animate={{opacity: 1}}
+           exit={{opacity: 0}}>
             <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+            </motion.div>
 ))}
+      </AnimatePresence>
     </div>
   )
 }

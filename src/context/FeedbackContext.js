c2ea505178b311d42d924097df5a7776a806268b
rob oren <acalltoredemption@gmail.com>
2022-05-26 12:39:01 -0400

    update item and delete all functioning to our mock backend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -25,8 +25,9 @@ const fetchFeedback = async () => {
     setIsLoading(false)
 }
 //delete feedback
-        const deleteFeedback = (id) => {
+        const deleteFeedback = async (id) => {
             if(window.confirm('Are you sure you want to delete?')){
+                await fetch(`/feedback/${id}`, {method: 'DELETE'})
                 setFeedback(feedback.filter((item) => item.id !== id ))
             }
         }
@@ -53,8 +54,17 @@ const fetchFeedback = async () => {
             })
         }
 //upodate feedback item
-        const updateFeedback = (id, updItem) => {
-            setFeedback(feedback.map((item) => item.id === id ? {...item, ...updItem} : item))
+        const updateFeedback = async (id, updItem) => {
+            const response = await fetch( `/feedback/${id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(updItem)
+            })
+
+            const data = await response.json()
+            setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item))
         }
 
     return <FeedbackContext.Provider value={{
